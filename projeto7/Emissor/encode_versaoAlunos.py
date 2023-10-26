594f4e76ce7c2b9452e0daedcdfe9552b0d54fd2
Thiago Victoriano <thiagovic03@gmail.com>
2023-10-26 11:05:49 -0300

    emissor

--- projeto7/Emissor/encode_versaoAlunos.py
+++ projeto7/Emissor/encode_versaoAlunos.py
@@ -4,11 +4,29 @@ from suaBibSignal import *
 import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.fftpack import fft, fftshift
 
 #funções a serem utilizadas
 def signal_handler(signal, frame):
         print('You pressed Ctrl+C!')
         sys.exit(0)
+
+def generateSin(freq, time, fs):
+    n = time*fs #numero de pontos
+    x = np.linspace(0.0, time, n)  # eixo do tempo
+    s = np.sin(freq*x*2*np.pi)
+    
+    return (x, s)
+
+def calcFFT(signal, fs):
+    # https://docs.scipy.org/doc/scipy/reference/tutorial/fftpack.html
+    #y  = np.append(signal, np.zeros(len(signal)*fs))
+    N  = len(signal)
+    T  = 1/fs
+    xf = np.linspace(-1.0/(2.0*T), 1.0/(2.0*T), N)
+    yf = fft(signal)
+    return(xf, fftshift(yf))
 
 #converte intensidade em Db, caso queiram ...
 def todB(s):
@@ -33,19 +51,52 @@ def main():
     # Grave o som com seu celular ou qualquer outro microfone. Cuidado, algumas placas de som não gravam sons gerados por elas mesmas. (Isso evita microfonia).
     
     # construa o gráfico do sinal emitido e o gráfico da transformada de Fourier. Cuidado. Como as frequencias sao relativamente altas, voce deve plotar apenas alguns pontos (alguns periodos) para conseguirmos ver o sinal
+
+    NUM = input('Digite um número entre 0 e 9: ')
+    dic_freq = {
+         '0': (1336, 941),
+         '1': (1209, 697),
+         '2': (1336, 697),
+         '3': (1477, 697),
+         '4': (1209, 770),
+         '5': (1336, 770),
+         '6': (1477, 770),
+         '7': (1209, 852),
+         '8': (1336, 852),
+         '9': (1477, 852)
+    }
     
+    fs = 44100
+    T = 4
+    freq1 = dic_freq[NUM][0]
+    freq2 = dic_freq[NUM][1]
+
+    x1, y1 = generateSin(freq1,T,fs)
+    x2, y2 = generateSin(freq2,T,fs)
+    y = y1 + y2
+    y = y/max(y)
+    t = np.linspace(0,T,T*fs)
+
+    plt.figure()
+    plt.plot(t, y, '.-')
+    plt.xlim(0, 0.003)
+
+    X, Y = calcFFT(y,fs)
+    plt.figure()
+    plt.stem(X,np.abs(Y))
+    plt.xlim(-10000, 10000)
 
     print("Inicializando encoder")
     print("Aguardando usuário")
     print("Gerando Tons base")
     print("Executando as senoides (emitindo o som)")
     print("Gerando Tom referente ao símbolo : {}".format(NUM))
-    sd.play(tone, fs)
+    sd.play(y, fs)
     # Exibe gráficos
     plt.show()
     # aguarda fim do audio
     sd.wait()
-    plotFFT(self, signal, fs)
+
     
 
 if __name__ == "__main__":
